6504279e05cd44098d78c5d58d27b7cfbeeafae7
Royston Monteiro <rpm319@nyu.edu>
2014-12-17 22:51:37 -0500

    Bare bones recommendation. Finding similality between a pair of users

--- recommendations.py
+++ recommendations.py
@@ -1,24 +1,5 @@
 __author__ = 'Royston'
-# #NOTE : copied verbatim from the paper : Predictive Collective Intelligence by Toby Segaran.
-# A dictionary of movie critics and their ratings of a small
-# set of movies
-critics = {'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
-                         'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5,
-                         'The Night Listener': 3.0},
-           'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5,
-                            'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0,
-                            'You, Me and Dupree': 3.5},
-           'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
-                                'Superman Returns': 3.5, 'The Night Listener': 4.0},
-           'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
-                            'The Night Listener': 4.5, 'Superman Returns': 4.0,
-                            'You, Me and Dupree': 2.5},
-           'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
-                            'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
-                            'You, Me and Dupree': 2.0},
-           'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
-                             'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
-           'Toby': {'Snakes on a Plane': 4.5, 'You, Me and Dupree': 1.0, 'Superman Returns': 4.0}}
+# Data from http://grouplens.org/datasets/movielens/
 from math import sqrt
 # Returns the Pearson correlation coefficient for p1 and p2
 # Would ideally use a well tested library for things like this. But copying the function vertabim from the book for now
@@ -48,43 +29,73 @@ def sim_pearson(prefs, p1, p2):
 
 
 def import_data():
-    data = dict();
+    data = dict()
     with open('T:/data/datasets/movieLens/ml-100k/u1.base') as inf:
         for line in inf:
-            words = line.split();
-            words.pop(); #Discard the last word
-            rating_number = int(words.pop());
-            movie = words.pop();
-            user_name = words.pop();
-            if user_name in data:
-                data[user_name][movie] = rating_number;
-            else:
-                data[user_name] = dict();
-                data[user_name][movie] = rating_number;
+            words = line.split()
+            words.pop() #Discard the last word
+            rating_number = int(words.pop())
+            movie = words.pop()
+            user_name = words.pop()
+            if not user_name in data:
+                data[user_name] = dict()
+            data[user_name][movie] = rating_number
     return data
 
 
 allRatings = import_data()
-print(sim_pearson(allRatings, '1', '2'));
 
 
-import operator;
+import operator
+from operator import itemgetter
 
+from collections import OrderedDict
 
-def get_top_similar_users(userNo):
-    similarities = dict();
+def get_top_similar_users(userNo, n=20):
+    similarities = dict()
     for user in allRatings.keys():
         if user == userNo:
-            continue;
-        print(user);
-        similarities[user] =  sim_pearson(allRatings, userNo, user);
-    sorted_x = sorted(similarities.items(), key=operator.itemgetter(1), reverse=True)
-    print(sorted_x)
-    return sorted_x;
+            continue
+        similarities[user] = sim_pearson(allRatings, userNo, user)
+    return OrderedDict(sorted(similarities.items(), key=itemgetter(1), reverse=True))
 
 # example usage. Build these most similar users to find a weighted average rank for every movie.
 # -> More similar the user, the more weightage for the movies the user has watched to the final recommendation
-sortedx = get_top_similar_users('1');
+def recommend(user):
+    # Weighted average of the ranking that the most similar users
+    users_movies = allRatings[user].keys()
+    movie_to_total_similarity = dict()
+    movie_to_weighted_rating = dict()
+    top_similar_users = get_top_similar_users(user, 15)
+    print(top_similar_users)
+    for sim_user in top_similar_users.keys():
+        similarity = top_similar_users[sim_user];
+        for movie in allRatings[sim_user].keys():
+            # Skip movies the user has already watched
+            if movie in users_movies:
+                continue
+            if not movie in movie_to_total_similarity:
+                movie_to_total_similarity[movie] = 0
+            movie_to_total_similarity[movie] = movie_to_total_similarity[movie] + similarity
+
+            if not movie in movie_to_weighted_rating:
+                movie_to_weighted_rating[movie] = 0
+            movie_to_weighted_rating[movie] = movie_to_weighted_rating[movie] + (allRatings[sim_user][movie] * similarity)
+            if movie == '1622':
+                print(movie, sim_user, allRatings[sim_user][movie], similarity)
+    movie_recommendations = dict();
+    for movie in movie_to_total_similarity:
+        if movie_to_total_similarity[movie] == 0:
+            continue
+        movie_recommendations[movie] = movie_to_weighted_rating[movie] / movie_to_total_similarity[movie];
+        #if movie_recommendations[movie] > 5:
+        #    print(movie, movie_to_weighted_rating[movie],  movie_to_total_similarity[movie], movie_recommendations[movie])
+    return OrderedDict(sorted(movie_recommendations.items(), key=itemgetter(1), reverse=True))
+
+print(recommend('1'))
+
+
+
 # Find the pearson distances from this user to every other user
 
 
